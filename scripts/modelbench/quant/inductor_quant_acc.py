47fb69042390fcd78346884fbd43385a7bbf3b89
chunhuanMeng <105194461+chunhuanMeng@users.noreply.github.com>
2025-09-01 09:48:09 +0800

    Update inductor_quant_acc.py

--- scripts/modelbench/quant/inductor_quant_acc.py
+++ scripts/modelbench/quant/inductor_quant_acc.py
@@ -64,6 +64,8 @@ def run_model(model_name, args):
         print("using cpp_wrapper")
         torchinductor.config.cpp_wrapper = args.cpp_wrapper
     valdir = args.dataset_dir
+    import pdb
+    pdb.set_trace()
     normalize = transforms.Normalize(
         mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
     )
@@ -95,6 +97,7 @@ def run_model(model_name, args):
     quant_top1 = AverageMeter("Acc@1", ":6.2f")
     quant_top5 = AverageMeter("Acc@5", ":6.2f")
     x = torch.randn(50, 3, 224, 224).contiguous(memory_format=torch.channels_last)
+
     if args.dtype != "float32":
         dtype = getattr(torch, args.dtype)
         x = x.to(dtype)
@@ -174,6 +177,9 @@ def run_model(model_name, args):
     with torch.no_grad():
         for i, (images, target) in enumerate(val_loader):
             images = images.to(args.device)
+            if args.dtype != "float32":
+                dtype = getattr(torch, args.dtype)
+                images = images.to(dtype)
             target = target.to(args.device)
             quant_output = optimized_model(images)
             quant_acc1, quant_acc5 = accuracy(quant_output, target, topk=(1, 5))
